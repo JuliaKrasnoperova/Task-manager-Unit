52760e2daadc802b83b499ab9f2c4384e5896d67
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2023-07-08 14:23:32 +0500

    preloader has been added

--- js/main.js
+++ js/main.js
@@ -1,35 +1,36 @@
+window.onload = function () {
+  let preloader = document.getElementById("preloader");
+  preloader.style.display = "none";
+};
+
 // Работа с навигацией
-const navIcon = document.querySelector('.nav-icon');
-const nav = document.querySelector('.nav');
+const navIcon = document.querySelector(".nav-icon");
+const nav = document.querySelector(".nav");
 
-
-navIcon.addEventListener('click', function () {
-    this.classList.toggle('nav-icon--active');
-    nav.classList.toggle('nav--active')
+navIcon.addEventListener("click", function () {
+  this.classList.toggle("nav-icon--active");
+  nav.classList.toggle("nav--active");
 });
 
-
 // Находим ссылки внутри мобильной навигации
-const navLinks = document.querySelectorAll('.nav a');
+const navLinks = document.querySelectorAll(".nav a");
 // Обходим ссылки методом forEach
 navLinks.forEach(function (item) {
-    // Для каждой ссылки добавляем прослушку по событию "клик"
-    item.addEventListener('click', function () {
-        navIcon.classList.remove('nav-icon--active'); // Убираем активный класс у иконки мобильной навигации
-        nav.classList.remove('nav--active'); // Убираем активный класс у блока мобю навигации
-    })
-})
+  // Для каждой ссылки добавляем прослушку по событию "клик"
+  item.addEventListener("click", function () {
+    navIcon.classList.remove("nav-icon--active"); // Убираем активный класс у иконки мобильной навигации
+    nav.classList.remove("nav--active"); // Убираем активный класс у блока мобю навигации
+  });
+});
 
 // Работа с формой поиска
-const searchForm = document.querySelector('.search');
-const searchButton = document.querySelector('.search__button');
+const searchForm = document.querySelector(".search");
+const searchButton = document.querySelector(".search__button");
 
-searchButton.addEventListener('click', function (e) {
-    if (searchForm.classList.contains('search--visible')) {
-
-    } else {
-        e.preventDefault(); // отмена стандартного поведения формы для отправки данных
-        searchForm.classList.toggle('search--visible');
-    }
-
-})
+searchButton.addEventListener("click", function (e) {
+  if (searchForm.classList.contains("search--visible")) {
+  } else {
+    e.preventDefault(); // отмена стандартного поведения формы для отправки данных
+    searchForm.classList.toggle("search--visible");
+  }
+});
